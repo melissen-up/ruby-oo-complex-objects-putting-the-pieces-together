8949b6bc2203401d600b6a894986eb70a9df9f31
Melissa Faulner <melissa.faulner@gmail.com>
2020-12-13 13:17:54 -0500

    commit tree 6cf9a18c723ae746b807e786769a3f2acdc6c739

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+class Book
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title_param)
+        @title = title_param
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# binding.pry
+# 0
